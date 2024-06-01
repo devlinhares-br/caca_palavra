efae4f28089e1b9006942b7b769c3a53e14b35c7
Lucas Linhares <lucas.linhares15@outlook.com>
2024-06-01 10:59:04 -0300

    Update gera_imagem.py

--- gera_imagem.py
+++ gera_imagem.py
@@ -16,7 +16,7 @@ class GeraImagem:
 
         img_width = tamanho * cell_size + 2 * margin
         img_height = tamanho * cell_size + 2 * margin + 100
-        img = Image.new('RGB', (img_width, img_height), color='white')
+        img = Image.new('RGBA', (img_width, img_height), color=(0, 0, 0, 0)) # Deixa transparente
         draw = ImageDraw.Draw(img)
 
         try:
@@ -29,19 +29,6 @@ class GeraImagem:
             title_font = ImageFont.load_default()
             fonte_palavras = ImageFont.load_default()
 
-        x0 = margin
-        y0 = margin + 100  # Ajuste para posicionar abaixo do título
-        x1 = x0 + tamanho * cell_size
-        y1 = y0 + tamanho * cell_size
-        draw.rectangle([x0, y0, x1, y1], outline='black')
-
-        for i in range(tamanho):
-            for j in range(tamanho):
-                x = margin + j * cell_size
-                y = margin + 100 + i * cell_size  # Ajuste para posicionar abaixo do título
-                if self.grade[i][j] != ' ':
-                    draw.text((x + 7, y + 5), self.grade[i][j], fill='black', font=font)
-
         # Escrever o título
         title_bbox = draw.textbbox(xy=(0, 0), text=self.titulo, font=title_font)
         title_width = title_bbox[2] - title_bbox[0]
@@ -52,20 +39,40 @@ class GeraImagem:
         current_line = ""
         y_offset = 100  # Posição inicial do texto acima do caça-palavras
 
+        lines = []
+
         for palavra in self.palavras_adicionadas:
             new_line = current_line + (palavra + " - ")
             text_bbox = draw.textbbox(xy=(0, 0), text=new_line, font=fonte_palavras)
             text_width = text_bbox[2] - text_bbox[0]
 
             if text_width > max_width:
-                draw.text((margin, y_offset), current_line, fill='black', font=fonte_palavras)
-                y_offset += 30
+                lines.append(current_line)
                 current_line = palavra + " - "
             else:
                 current_line = new_line
         current_line = current_line[:-2]
         if current_line:
-            draw.text((margin, y_offset), current_line, fill='black', font=fonte_palavras)
+            lines.append(current_line)
+
+        for line in lines:
+            draw.text((margin, y_offset), line, fill='black', font=fonte_palavras)
+            y_offset += 30
+
+        # Ajuste da posição inicial da grade com base na quantidade de linhas de palavras
+        y0 = y_offset + 20
+        x0 = margin
+        x1 = x0 + tamanho * cell_size
+        y1 = y0 + tamanho * cell_size
+
+        draw.rectangle([x0, y0, x1, y1], outline='black')
+
+        for i in range(tamanho):
+            for j in range(tamanho):
+                x = margin + j * cell_size
+                y = y0 + i * cell_size
+                if self.grade[i][j] != ' ':
+                    draw.text((x + 7, y + 5), self.grade[i][j], fill='black', font=font)
 
         img.save(nome_arquivo)
 
